156ba658aa3fecfdb2efcd210315c11b8bdc5a9d
Duong Thanh Hop <hop.duongthanh@gameloft.com>
2017-09-05 16:18:24 +0700

    delete new version

--- commitSVN.bat
+++
@@ -1,1 +0,0 @@
-svn commit -m"!auto backup database"
--- windows/templatetags/windows_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-import hashlib
-
-register = template.Library()
-
-@register.filter(name='gavatar')
-def gavatar(email, size):
-    return 'https://www.gravatar.com/avatar/{0}?d=identicon&s={1}'.format(hashlib.md5(email.encode('utf-8')).hexdigest(), size)
